0c2c756cc8d4752bda12e9f396621c0ed94b3a8a
swatiiii2803 <patilswatisagar@gmail.com>
2025-06-13 14:06:34 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,11 @@
+public class fibonacci {
+    public static void main(String[] args){
+        int number=10;
+        int fibonacci=0;
+        for(int i=1;i<=number;i++){
+            fibonacci=fibonacci+i;
+        }
+        System.out.println("fibonacci sequence is" + fibonacci);
+
+    }
+}
